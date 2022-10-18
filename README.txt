fc2f2efa05aabfe39001454928045470e7a12c15
NicolasErazo <nico.lacho@outlook.com>
2022-10-18 14:30:28 -0500

    ok

---
+++ README.txt
@@ -0,0 +1,26 @@
+CREDITS:
+
+Bootstrap
+http://getbootstrap.com/
+
+jQuery
+http://jquery.com/
+
+OwlCarousel
+https://owlcarousel2.github.io/OwlCarousel2/
+
+Isotope
+https://isotope.metafizzy.co
+
+Select2
+https://select2.org
+
+Stellar
+http://markdalgleish.com/projects/stellar.js/
+
+Lightcase
+https://cornel.bopp-art.com/lightcase/
+
+Demo Images:
+http://unsplash.com
+
---
+++ src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template, request, redirect
+import smtplib
+from decouple import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('/index.html')    
+
+@app.route('/about')
+def about():
+    return render_template('/about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('/blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('/contact.html')  
+
+@app.route('/contact/save', methods=['POST'])
+def save_contact_and_send_email():
+
+    name =  request.form['name']
+    email =  request.form['email']
+    phoneNumber =  request.form['phoneNumber']
+    message =  request.form['message']
+    subject = "Contact Us"
+
+    messageEmail = 'Subject: {}\n\n {} quiere contactarse contigo a travez del correo: {}\n\nSu numero de contacto es: {}\n\nSu mensaje: {}'.format(subject, name, email, phoneNumber, message)
+
+    server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    server.starttls()
+    server.login("nico.lacho@outlook.com", config('SECRET_KEY'))
+    server.sendmail("nico.lacho@outlook.com", "nico.lacho@outlook.com", messageEmail)
+    server.quit()
+
+    return redirect('/') 
+
+if __name__ == '__main__':
+    app.run(debug=True)      
